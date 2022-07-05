dcc9a0fe2515f3fbea1954d1cfe6422d2a3eaf3b
Medas <medas02345@gmail.com>
2022-07-05 21:14:31 +0300

    Add basic math functions

---
+++ javascript.js
@@ -0,0 +1,19 @@
+function add(a, b){
+    return a + b;
+}
+
+function subtract(a, b){
+    return a - b;
+}
+
+function multiply(a, b){
+    return a * b;
+}
+function divide(a, b){
+    return a / b;
+}
+
+console.log(add(5,6));
+console.log(subtract(125, 5));
+console.log(multiply(15,5));
+console.log(divide(15,5));
