2c5f6772f5f11e8e574cccc56b044910a7abe736
Medas <medas02345@gmail.com>
2022-07-06 21:02:24 +0300

    Add a displayOperator function, that allows operators to be shown on the screen after click

--- javascript.js
+++ javascript.js
@@ -50,12 +50,22 @@ function getDisplayValue(){
     return document.querySelector("#display").textContent;
 }
 
-/** 
-function displayAdd(){
+function displayOperator(operator){
     const display = document.querySelector("#display");
-    if(display.textContent);
+    
+    let lastChar = display.textContent[display.textContent.length-1];
+    
+    if( (lastChar !== operator) && (Number.isInteger(parseInt(lastChar)) === false)){
+        console.log(lastChar);
+        let displayString = display.textContent;
+        let str = displayString.slice(0, -1);
+        display.textContent = str + operator;
+    }
+    else if(Number.isInteger(parseInt(lastChar)) === true){
+        
+        display.textContent += operator;
+    }
 }
-*/
 
 
 
